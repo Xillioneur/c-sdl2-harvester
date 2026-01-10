9d37adf568f562f1af0920e608c19e6333b29685
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2026-01-10 12:49:22 -0500

    Initialize repo and add main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    return 0;
+}
