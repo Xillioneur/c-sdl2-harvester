03335d7663c8101624a9bd2b7503bcf704dd64d7
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2026-01-10 15:20:39 -0500

    Define main method with a window, renderer, and init_game()

--- main.c
+++ main.c
@@ -6,7 +6,36 @@
 #include <time.h>
 #include <stdbool.h>
 
+#define WINDOW_W 1200
+#define WINDOW_H 675
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+void init_game() {
+    srand(time(NULL));
+};
+
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Nebula Harvester", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_W, WINDOW_H, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+    init_game();
+
+    bool running = true;
+    while (running) { 
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            if (ev.type == SDL_QUIT) running = false;
+        }
+
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
